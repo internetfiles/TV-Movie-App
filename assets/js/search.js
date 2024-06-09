618df69ab230bc065fbe5bc41b6ff9d95e731fe9
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-06-09 11:32:54 +0300

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,7 @@
+"use strict";
+
 import { api_key, fetchDataFromServer } from "./api.js";
 import { createMovieCard } from "./movie-card.js";
-import { createTvShowCard } from "./tv-show-card.js"; // Assuming you have a similar function for TV show cards
 import { sidebar } from "./sidebar.js";
 
 export function search() {
@@ -25,45 +26,31 @@ export function search() {
     clearTimeout(searchTimeout);
 
     searchTimeout = setTimeout(function () {
-      // Search for both movies and TV shows
-      Promise.all([
-        fetchDataFromServer(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&page=1&include_adult=false&query=${searchField.value}`),
-        fetchDataFromServer(`https://api.themoviedb.org/3/search/tv?api_key=${api_key}&page=1&include_adult=false&query=${searchField.value}`)
-      ]).then(([movieResponse, tvResponse]) => {
-        const movieList = movieResponse.results;
-        const tvList = tvResponse.results;
+      fetchDataFromServer(
+        `https://api.themoviedb.org/3/search/multi?api_key=${api_key}&page=1&include_adult=false&query=${searchField.value}`,
+        function ({ results: movieList }) {
+          searchWrapper.classList.remove("searching");
+          searchResultModal.classList.add("active");
+          searchResultModal.innerHTML = ""; //remove old results
 
-        searchWrapper.classList.remove("searching");
-        searchResultModal.classList.add("active");
-        searchResultModal.innerHTML = ""; //remove old results
+          searchResultModal.innerHTML = `
+            <p class="label">Result for</p>
+            <h1 class="heading">${searchField.value}</h1>
 
-        searchResultModal.innerHTML = `
-          <p class="label">Result for</p>
-          <h1 class="heading">${searchField.value}</h1>
+            <div class="movie-list">
+              <div class="grid-list"></div>
+            </div>
+          `;
 
-          <div class="result-list">
-            <div class="grid-list"></div>
-          </div>
-        `;
+          for (const movie of movieList) {
+            const movieCard = createMovieCard(movie);
 
-        // Display movie cards
-        for (const movie of movieList) {
-          const movieCard = createMovieCard(movie);
-          searchResultModal
-            .querySelector(".grid-list")
-            .appendChild(movieCard);
+            searchResultModal
+              .querySelector(".grid-list")
+              .appendChild(movieCard);
+          }
         }
-
-        // Display TV show cards
-        for (const show of tvList) {
-          const showCard = createTvShowCard(show);
-          searchResultModal
-            .querySelector(".grid-list")
-            .appendChild(showCard);
-        }
-      }).catch(error => {
-        console.error("Error fetching data:", error);
-      });
+      );
     }, 500);
   });
 }
