0c45e0e96c3be97b45b9dbe69cdab26d961781ea
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-06-10 11:42:41 +0300

    Update detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -169,7 +169,6 @@ const playMovieButton = document.createElement("button");
 playMovieButton.textContent = "Play Movie";
 playMovieButton.style.border = "2px solid navy";
 playMovieButton.style.backgroundColor = "transparent";
-playMovieButton.style.color = "navy";
 playMovieButton.style.padding = "10px 20px";
 playMovieButton.classList.add("play-movie-button", "title-large", "title-wrapper");
 
